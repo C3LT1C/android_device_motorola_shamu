bb9cb3ecc773c3e7eb99e14b6af7a2259187c070
Snuzzo <jrracinfan@gmail.com>
2016-02-17 03:25:32 -0500

    Initial Commit

---
+++ Android.mk
@@ -0,0 +1,5 @@
+ifneq ($(filter shamu,$(RENDER_PRODUCT)),)
+
+include $(DEVICE_MAKEFILE)
+
+endif
---
+++ render.mk
@@ -0,0 +1,12 @@
+include vendor/render/configs/common.mk
+
+PRODUCT_NAME := render_shamu
+PRODUCT_DEFCONFIG := B14CKB1RD_defconfig
+PRODUCT_KERNEL_SOURCE := kernel/moto/shamu
+CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.3/bin/arm-eabi-
+ARCH := arm
+
+# Override default zImage define
+TARGET_ZIMAGE := zImage-dtb
+
+ZIP_FILES_DIR := device/motorola/shamu/anykernel_installer
