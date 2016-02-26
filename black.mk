56b1b7416326f6e884096b92ceef2ff1d4791968
Snuzzo <jrracinfan@gmail.com>
2016-02-26 02:06:17 -0500

    Fixes for toolchain selection regarding brands

--- black.mk
+++ black.mk
@@ -1,9 +1,10 @@
-include vendor/black/configs/common.mk
-
 PRODUCT_NAME := black_shamu
 PRODUCT_DEFCONFIG := B14CKB1RD_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/moto/shamu
 ARCH := arm
+
+# Include Kernel Vendor
+include vendor/black/configs/common.mk
 
 # Override default zImage define
 TARGET_ZIMAGE := zImage-dtb
